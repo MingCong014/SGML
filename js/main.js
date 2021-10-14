7aca49337188c4859365ee019235e20fabe168b9
MingCong014 <46553550+MingCong014@users.noreply.github.com>
2021-10-14 22:42:58 +0800

    Testimonial Block

--- js/main.js
+++ js/main.js
@@ -4,3 +4,7 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+document.querySelector(".banner__close").addEventListener("click", function () {
+  this.closest(".banner").style.display = "none";
+});
