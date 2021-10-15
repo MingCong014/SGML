cb5d15403ffa63e6c57464d4e3a52cbff83372f7
MingCong014 <46553550+MingCong014@users.noreply.github.com>
2021-10-16 02:22:31 +0800

    Cmobining part one and bug fixes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
+/* For clicking collapsible menu */
 const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
     this.classList.toggle("collapsible--expanded");
   })
 );
+/* End */
+
+
+/* Closing Top Banner */
 
 document.querySelector(".banner__close").addEventListener("click", function () {
   this.closest(".banner").style.display = "none";
 });
+ 
+/*  
+//Not working yet
+const transparent = document.querySelector(".transparent");
+window.onscroll = function () {
+  var top = window.scrollY;
+  console.log(top);
+  if (top >= 100) {
+    nav.classList.add("active")
+  } else {
+    nav.classList.remove("active");
+  }
+};
+
+*/
