cfcc67b2d70464df3ed2b1d4f20fe3ed946ccfb8
MingCong014 <46553550+MingCong014@users.noreply.github.com>
2021-10-18 00:55:39 +0800

    Wrapping up changes and bug-fixes

--- js/main.js
+++ js/main.js
@@ -7,13 +7,27 @@ collapsibles.forEach((item) =>
 );
 /* End */
 
-
 /* Closing Top Banner */
-
 document.querySelector(".banner__close").addEventListener("click", function () {
   this.closest(".banner").style.display = "none";
 });
- 
+
+/* Apply page drop-down plan list */
+const selected = document.querySelectorAll(".selected");
+const optionsContainer = document.querySelectorAll(".options-container");
+const optionsList = document.querySelectorAll(".option");
+
+selected.addEventListener("click", () => {
+  optionsContainer.classList.toggle("select-active");
+});
+
+optionsList.forEach((o) => {
+  o.addEventListener("click", () => {
+    selected.innerHTML = o.querySelector("label").innerHTML;
+    optionsContainer.classList.remove("select-active");
+  });
+});
+
 /*  
 //Not working yet
 const transparent = document.querySelector(".transparent");
